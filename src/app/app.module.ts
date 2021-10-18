e032881ef499731bb957cc2d5ed3238718021373
thalysmcarrara <thalysmatiascarrara@gmail.com>
2021-10-18 11:52:38 -0400

    create product form with two way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,9 @@ import { ProductCreateComponent } from './components/product/product-create/prod
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { HttpClientModule } from '@angular/common/http'
-
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 @NgModule({
   declarations: [
     AppComponent,
@@ -45,6 +47,9 @@ import { HttpClientModule } from '@angular/common/http'
     MatButtonModule,
     MatSnackBarModule,
     HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
